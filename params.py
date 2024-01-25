d2c2de6d9609c63f9c1482217cfb07b132510ada
Bhavesh Badaya <bhavesh.badaya.95@gmail.com>
2024-01-25 18:24:11 +0530

    Initial commit

---
+++ params.py
@@ -0,0 +1,6 @@
+openai_api_key = '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'
+
+mongodb_conn_string = 'mongodb+srv://searchdb:b9Aj57jBfKxhyvhz@searchdb.jfge7ij.mongodb.net/?retryWrites=true&w=majority'
+db_name = "searchdb"
+collection_name = "search_col"
+index_name = "vsearch_index"
